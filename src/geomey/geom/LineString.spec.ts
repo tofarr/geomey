b936bedbd8ffba73469191612ceca96ae3d6495c
Tim O'Farrell <tofarr@gmai.com>
2024-08-17 21:23:14 -0600

    LineString coverage at 100%

--- src/geomey/geom/LineString.spec.ts
+++ src/geomey/geom/LineString.spec.ts
@@ -1,8 +1,9 @@
 import * as chai from "chai";
-import { LineString } from "../geom";
+import { LineSegment, LineString } from "../geom";
 import { Tolerance } from "../Tolerance";
 import { InvalidCoordinateError } from "../coordinate";
 import { AffineTransformer } from "../transformer/AffineTransformer";
+import { A_OUTSIDE_B, B_OUTSIDE_A, TOUCH } from "../Relation";
 
 const expect = chai.expect;
 const TOLERANCE = new Tolerance(0.1);
@@ -41,6 +42,7 @@ export const lineStringSpec = () => {
     const lineString = new LineString([
       0, 0, 10, 0, 10, 2, 2, 2, 2, 8, 10, 8, 10, 10, 0, 10, 0, 0,
     ]);
+    expect(lineString.getLength()).to.equal(56);
     expect(lineString.getLength()).to.equal(56);
     expect(lineString.getCentroid().toWkt()).to.equal("POINT(4.1 5)");
   });
@@ -97,4 +99,10 @@ export const lineStringSpec = () => {
     );
     expect(lineString.generalize(new Tolerance(0.01))).to.equal(lineString)
   });
+
+  it("relates to line segments", () => {
+    const lineString = new LineString([20, 0, 20, 40, 60, 20]);
+    const lineSegment = new LineSegment(0, 0, 80, 80)
+    expect(lineString.relate(lineSegment, TOLERANCE)).to.equal(A_OUTSIDE_B | B_OUTSIDE_A | TOUCH)
+  })
 };
